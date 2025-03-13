88f97fd49b4996a231acb74cc36208c2bdbc7f12
Aleksashka301 <aleksashka301@gmail.com>
2025-03-13 19:58:27 +0500

    show_pokemon, теперь данные берутся из моделей

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -29,8 +29,6 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    # with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-    #     pokemons = json.load(database)['pokemons']
     current_time = localtime()
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
 
@@ -61,24 +59,25 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
-    for pokemon in pokemons:
-        if pokemon['pokemon_id'] == int(pokemon_id):
-            requested_pokemon = pokemon
-            break
+    if Pokemon.objects.get(id=int(pokemon_id)):
+        pokemon = Pokemon.objects.get(id=pokemon_id)
     else:
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
+    image_url = request.build_absolute_uri(pokemon.image.url)
+    requested_pokemon = {
+        'title_ru': pokemon.title,
+        'img_url': image_url,
+    }
+
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity in requested_pokemon['entities']:
+    for pokemon_entity in PokemonEntity.objects.filter(pokemon=pokemon):
         add_pokemon(
-            folium_map, pokemon_entity['lat'],
-            pokemon_entity['lon'],
-            pokemon['img_url']
+            folium_map, pokemon_entity.lat,
+            pokemon_entity.lon,
+            requested_pokemon['img_url'],
         )
 
     return render(request, 'pokemon.html', context={
-        'map': folium_map._repr_html_(), 'pokemon': pokemon
+        'map': folium_map._repr_html_(), 'pokemon': requested_pokemon
     })
