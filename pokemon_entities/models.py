bb58bfb845a3582b5b2421fa820c5c19b0716136
Aleksashka301 <aleksashka301@gmail.com>
2025-03-15 17:54:31 +0500

    Added field descriptions

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,17 +3,18 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, default='', help_text='Название на английском')
-    title_jp = models.CharField(max_length=200, default='', help_text='Название на японском')
-    image = models.ImageField(null=True)
-    description = models.TextField(default='', help_text='Введите описание покемона')
+    title = models.CharField('Имя покемона:', max_length=200)
+    title_en = models.CharField('Имя покемона на анг.:', max_length=200, default='', help_text='Название на английском')
+    title_jp = models.CharField('Имя покемона на яп.:', max_length=200, default='', help_text='Название на японском')
+    image = models.ImageField('Картинка покемона:', null=True)
+    description = models.TextField('Описание покемона:', default='', help_text='Введите описание покемона')
     previous_evolution = models.ForeignKey(
         'self',
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name='next_evolutions',
+        related_name='next_evolution',
+        verbose_name='Предок покемона:'
     )
 
     def __str__(self):
@@ -21,15 +22,15 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField()
-    lon = models.FloatField()
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон:')
+    lat = models.FloatField('Широта:')
+    lon = models.FloatField('Долгота:')
 
-    appeared_at = models.DateTimeField(default=timezone.now)
-    disappeared_at = models.DateTimeField(default=timezone.now)
+    appeared_at = models.DateTimeField('Дата и время появления покемона:', default=timezone.now)
+    disappeared_at = models.DateTimeField('Дата и время исчезновения покемона:', default=timezone.now)
 
-    level = models.IntegerField(default=1)
-    health = models.IntegerField(default=100)
-    strength = models.IntegerField(default=1)
-    defence = models.IntegerField(default=10)
-    stamina = models.IntegerField(default=5)
+    level = models.IntegerField('Уровень покемона:', default=1)
+    health = models.IntegerField('Жизнь покемона:', default=100)
+    strength = models.IntegerField('Сила покемона:', default=1)
+    defence = models.IntegerField('Защита покемона:', default=10)
+    stamina = models.IntegerField('Выносливость покемона:', default=5)
