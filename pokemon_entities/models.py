9a3dc20e391462722c9c90e9f20671f7b7975544
Aleksashka301 <aleksashka301@gmail.com>
2025-03-18 21:01:02 +0500

    Field importance is indicated

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,10 +4,10 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField('Имя покемона:', max_length=200)
-    title_en = models.CharField('Имя покемона на анг.:', max_length=200)
-    title_jp = models.CharField('Имя покемона на яп.:', max_length=200)
+    title_en = models.CharField('Имя покемона на анг.:', max_length=200, default='')
+    title_jp = models.CharField('Имя покемона на яп.:', max_length=200, default='')
     image = models.ImageField('Картинка покемона:', null=True)
-    description = models.TextField('Описание покемона:', default='')
+    description = models.TextField('Описание покемона:', default='', blank=True)
     previous_evolution = models.ForeignKey(
         'self',
         on_delete=models.SET_NULL,
@@ -22,15 +22,15 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон:')
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон:', related_name='entities')
     lat = models.FloatField('Широта:')
     lon = models.FloatField('Долгота:')
 
-    appeared_at = models.DateTimeField('Дата и время появления покемона:', default=timezone.now)
-    disappeared_at = models.DateTimeField('Дата и время исчезновения покемона:')
+    appeared_at = models.DateTimeField('Дата и время появления покемона:', default=timezone.now, null=True)
+    disappeared_at = models.DateTimeField('Дата и время исчезновения покемона:', null=True)
 
-    level = models.IntegerField('Уровень покемона:', default=1)
-    health = models.IntegerField('Жизнь покемона:', default=100)
-    strength = models.IntegerField('Сила покемона:', default=1)
-    defence = models.IntegerField('Защита покемона:', default=10)
-    stamina = models.IntegerField('Выносливость покемона:', default=5)
+    level = models.PositiveIntegerField('Уровень покемона:', default=1, blank=True)
+    health = models.PositiveIntegerField('Жизнь покемона:', default=100, blank=True)
+    strength = models.PositiveIntegerField('Сила покемона:', default=1, blank=True)
+    defence = models.PositiveIntegerField('Защита покемона:', default=10, blank=True)
+    stamina = models.PositiveIntegerField('Выносливость покемона:', default=5, blank=True)
